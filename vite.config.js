da41902c59e9c29a99be76155656af660ff82509
SanjayKumarSingh <sanjay.singh.1069@gmail.com>
2025-10-23 15:30:13 +0530

    Fixed port

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+        port: 5173, // ✅ your React dev port (can be 5173/5174)
+        proxy: {
+          // ✅ any request starting with /gateway-api will be forwarded to backend
+          "/student-api": {
+            target: "http://localhost:8081", // your Spring Boot backend
+            changeOrigin: true,
+            secure: false,
+          },
+        },
+      },
+    });
 })
